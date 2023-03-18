a8637dd5b5689e7e304de9e2530089790aa2ea53
Dmitriy Doronin <dmitriy435@gmail.com>
2023-03-18 17:58:11 +0400

    :recycle: Update nvm version

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,4 +1,6 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "$SCRIPT_DIR/../env"
 echo "Install nvm"
 sleep 1
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh | bash
 echo "Done"
