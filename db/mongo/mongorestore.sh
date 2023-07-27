9c7e3c675b4d4aadf35d69ecee8b35e1f440d67e
Dmitriy Doronin <dmitriy435@gmail.com>
2023-07-27 11:16:43 +0300

    :wrench: Update db compose

--- db/mongo/mongorestore.sh
+++ db/mongo/mongorestore.sh
@@ -1,3 +1,3 @@
 echo "[MONGO] Start mongorestore"
-docker exec mongo mongorestore --uri 'mongodb://root:example@localhost:27017'
+docker exec bitnami_mongodb mongorestore --uri 'mongodb://root:example@localhost:27017'
 echo "[MONGO] End mongorestore"
