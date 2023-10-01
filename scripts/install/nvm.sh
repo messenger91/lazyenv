ee5c5b52131b130e1909265d70640589c75165d4
Dmitriy Doronin <dmitriy435@gmail.com>
2023-10-01 17:39:26 +0300

    :wrench: Adjust postgres docker compose

--- scripts/install/nvm.sh
+++ scripts/install/nvm.sh
@@ -2,5 +2,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "$SCRIPT_DIR/../env"
 echo "Install nvm"
 sleep 1
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh | bash
 echo "Done"
