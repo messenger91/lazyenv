21683188aa4823786c6e259e949809dc08d79932
Dmitriy Doronin <dmitriy435@gmail.com>
2025-02-26 22:46:22 +0400

    :mute: Remove logs

--- scripts/utils/ssh-tunnel.sh
+++ scripts/utils/ssh-tunnel.sh
@@ -7,8 +7,6 @@ PORT=${3:-5432}
 IP=${4:-127.0.0.1}
 LOCAL_PORT=${5:-$((PORT+1))}
 
-echo PORT $PORT
-echo LOCAL_PORT $LOCAL_PORT
 kill -9 $(lsof -t -i tcp:$LOCAL_PORT)
 ssh -f -N -L localhost:$LOCAL_PORT:$IP:$PORT $USER@$HOST
 echo "SSH TUNNEL ESTABLISHED: localhost:$LOCAL_PORT:$IP:$PORT $USER@$HOST"
