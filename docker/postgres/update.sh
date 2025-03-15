78e5eba000a88d9ecfed1525ef63a65f3881d988
Dmitriy Doronin <dmitriy435@gmail.com>
2025-03-15 16:06:14 +0400

    :sparkles: Import dbeaver scripts

--- docker/postgres/update.sh
+++ docker/postgres/update.sh
@@ -5,4 +5,4 @@ docker cp $SCRIPT_DIR/pg_dump.sh $CONTAINER_ID:/pg_dump.sh
 docker cp $SCRIPT_DIR/pg_restore.sh $CONTAINER_ID:/pg_restore.sh
 docker cp $SCRIPT_DIR/psql_restore.sh $CONTAINER_ID:/psql_restore.sh
 docker cp $SCRIPT_DIR/.psqlrc $CONTAINER_ID:/root/.psqlrc
-docker cp $SCRIPT_DIR/scripts $CONTAINER_ID:/root/scripts
+docker cp $SCRIPT_DIR/scripts $CONTAINER_ID:/root
---
+++ scripts/utils/dbeaver-scripts.sh
@@ -0,0 +1,6 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "$SCRIPT_DIR/../../.env"
+
+echo "Copy DBeaver Scripts folder"
+cp -r ${DBEAVER_SCRIPTS_DIR}/*.sql docker/postgres/scripts
+echo "Done"
