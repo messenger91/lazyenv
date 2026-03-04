3adbd1d64bbba70024f9bfffd89ee6afc7d87263
Dmitriy Doronin <dmitriy435@gmail.com>
2026-03-04 15:09:48 +0300

    :zap: Add kafka

---
+++ docker/kafka/build.sh
@@ -0,0 +1,9 @@
+source "$(pwd)/../../.env"
+
+docker run -d \
+    --name kafka \
+    --network br0 \
+    --ip $DOCKER_CONTAINER_KAFKA_IP \
+    -p 9092:9092 \
+    -p 9093:9093 \
+        apache/kafka:$DOCKER_IMAGE_KAFKA_TAG
