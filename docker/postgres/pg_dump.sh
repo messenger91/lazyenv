4851004b33ca4d4e0444ea732a5ce4372b38e172
Dmitriy Doronin <dmitriy435@gmail.com>
2026-06-22 20:01:04 +0400

    :loud_sound: Log pg_dump

--- docker/postgres/pg_dump.sh
+++ docker/postgres/pg_dump.sh
@@ -5,5 +5,21 @@ PORT=5432
 USER=postgres
 
 FILE=$DATABASE\_$TIMESTAMP.dump
+
+START=$(date +%s)
 pg_dump -Fc --dbname=$DATABASE -U $USER -h $HOST -p $PORT -w -f /dump/$FILE
-echo "$FILE"
+EXIT_CODE=$?
+END=$(date +%s)
+
+ELAPSED=$((END - START))
+MINS=$((ELAPSED / 60))
+SECS=$((ELAPSED % 60))
+TIMER=$(printf "%d:%02d" $MINS $SECS)
+
+if [ $EXIT_CODE -eq 0 ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [OK] $FILE ${TIMER}" >> /dump/dump.log
+    echo "Dump completed: $FILE (${TIMER})"
+else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [FAIL] $DATABASE (exit $EXIT_CODE) ${TIMER}" >> /dump/dump.log
+    echo "Dump failed: $DATABASE (${TIMER})"
+fi
